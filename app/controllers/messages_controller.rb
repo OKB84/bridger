8e492346ce8acb758c3569a3dc84764eaeb9f9b1
okb <okb.for.git@gmail.com>
2019-03-10 18:14:44 +0000

    reply message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,10 +11,16 @@ class MessagesController < ApplicationController
   def new
     @message = Message.new
     @receive_user = User.find(params[:user_id])
+    unless @receive_user.lesson.present?
+      redirect_to messages_url
+    end
     @chosen_plan = LessonPlan.find_by(id: params[:plan_id])
     if @chosen_plan
       @default_message = 
       "はじめまして。\r#{current_user.name}と申します。\r\r以下の内容でレッスンを受講させていただきたいのですが、ご都合はいかがでしょうか。\r\r・場所：#{@chosen_plan.location_broad}\r・時間：#{@chosen_plan.minute}分\r・金額：#{@chosen_plan.price_yen.to_s(:delimited)}円\r・第一希望日時：●月●日（●）●●：●●開始\r・第二希望日時：●月●日（●）●●：●●開始\r・第三希望日時：●月●日（●）●●：●●開始"
+    end
+    if params[:reply_message]
+      @default_message = "\r\r>#{params[:reply_message]}"
     end
   end
 
@@ -34,7 +40,7 @@ class MessagesController < ApplicationController
   private
   
   def message_params
-    params.require(:message).permit(:content)
+    params.require(:message).permit(:content, :reply_message)
   end
   
 end
