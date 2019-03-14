c3c9371637402b347f7a37f1ecf54d13105029af
okb <okb.for.git@gmail.com>
2019-03-14 18:08:00 +0000

    modify lesson search form model

--- app/models/form/lesson_search_form.rb
+++ app/models/form/lesson_search_form.rb
@@ -9,7 +9,7 @@ class Form::LessonSearchForm
     rel = Lesson
     rel = Lesson.joins(:instructor).where(users: { name: name }) if name.present?
     rel = rel.joins(:available_instruments).where(available_instruments: { instrument_id: instrument_ids }) if instrument_ids.present?
-    rel = rel.joins(:available_subjects).where(available_subjects: { subject_id: subject_id }) if subject_ids.present?
+    rel = rel.joins(:available_subjects).where(available_subjects: { subject_id: subject_ids }) if subject_ids.present?
     rel = rel.joins(:lesson_plans).where(lesson_plans: { location_broad: location_broad }) if location_broad.present?
     rel = rel.joins(instructor: :users_languages).where(users_languages: { language_id: language_ids }) if language_ids.present?
     rel
