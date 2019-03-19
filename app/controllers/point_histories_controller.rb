07a8a69503134be2056dd4086ca88d245bf77ebb
okb <okb.for.git@gmail.com>
2019-03-19 09:34:47 +0000

    add retry to buying points

--- app/controllers/point_histories_controller.rb
+++ app/controllers/point_histories_controller.rb
@@ -12,13 +12,30 @@ class PointHistoriesController < ApplicationController
   
   def purchase
     Payjp.api_key = ENV['PAYJP_SECRET_KEY']
-    charge = Payjp::Charge.create(        #一旦newをしてif saveとした方がよい？chargeは何につかう変数なのか
-      :amount => params[:price],          #レッスンプランごとに変更
+    charge = Payjp::Charge.create(        
+      :amount => params[:price],          #レッスンプランごとに変更したいがnumber_fieldから数値を受け取れなかった
       :card => params['payjp-token'],
       :currency => 'jpy',
     )
-    Point.create(user_id: current_user.id, current_point: params[:price])
-    flash[:success] = 'ポイント購入が完了しました'
-    redirect_to point_histories_url
+    
+    try = 0
+  
+    PointHistory.transaction do
+      try += 1
+      PointHistory.create(user_id: nil, other_user_id: current_user.id, amount: params[:price])
+      
+      send_sum = PointHistory.where(user_id: current_user.id).sum(:amount)
+      receive_sum = PointHistory.where(other_user_id: current_user.id).sum(:amount)
+      current_point = receive_sum - send_sum
+      
+      Point.create(user_id: current_user.id, current_point: current_point)
+    end
+      flash[:success] = 'ポイントを購入しました'
+      redirect_to current_user
+    rescue => e
+      retry if try < 100
+      logger.error('purchased but not charged')
+      flash.now[:danger] = 'ポイントを購入できませんでした'
+      redirect_to current_user
   end
 end
