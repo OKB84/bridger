b9155f9dd0322bc52a57ea0f502944dd53bf9458
okb <okb.for.git@gmail.com>
2019-03-21 05:34:50 +0000

    withdraw form

--- app/controllers/withdraws_controller.rb
+++ app/controllers/withdraws_controller.rb
@@ -1,5 +1,10 @@
 class WithdrawsController < ApplicationController
   def new
+    @user = current_user
+    unless @user.bank.present? && @user.branch.present? && @user.account_type.present? && @user.account_number.present? && @user.account_holder.present?
+      flash[:warning] = '口座情報を入力してください'
+      redirect_to edit_user_url(current_user)
+    end
   end
 
   def create
