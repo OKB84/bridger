a919df4f367c12017ef97799a5150d6965305136
okb <okb.for.git@gmail.com>
2019-03-21 08:38:13 +0000

    languages cant be blank error when register user

--- app/models/users_language.rb
+++ app/models/users_language.rb
@@ -2,6 +2,6 @@ class UsersLanguage < ApplicationRecord
   belongs_to :user
   belongs_to :language
   
-  validates :user, presence: true
+  # validates :user, presence: true　これを入れるとUserの新規登録時にuser_idが未定の状態でusers_languageレコードを登録しないといけないため、エラーになってしまう。
   validates :language, presence: true
 end
