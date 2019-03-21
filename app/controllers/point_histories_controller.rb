e8f88ed0b74af88ff019c0dd3090884803a687bc
okb <okb.for.git@gmail.com>
2019-03-21 16:41:52 +0000

    point history index

--- app/controllers/point_histories_controller.rb
+++ app/controllers/point_histories_controller.rb
@@ -2,6 +2,7 @@ class PointHistoriesController < ApplicationController
   require 'payjp'
 
   def index
+    @point_histories = PointHistory.where("(from_user_id = ?) OR (to_user_id = ?)", current_user.id, current_user.id).order(created_at: 'DESC')
   end
 
   def new
