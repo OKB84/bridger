ef961ebbc4fe8fcb7d01557e6b2f2e59a6905e5d
okb <okb.for.git@gmail.com>
2019-03-24 14:25:20 +0000

    latest instructors on toppage

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,5 +1,8 @@
 class ToppagesController < ApplicationController
   skip_before_action :require_login
   def index
+    @latest_lesson = Lesson.last
+    @second_latest_lesson = Lesson.last(2)[0]
+    @third_latest_lesson = Lesson.last(3)[0]
   end
 end
--- app/controllers/withdraws_controller.rb
+++ app/controllers/withdraws_controller.rb
@@ -2,7 +2,7 @@ class WithdrawsController < ApplicationController
   def new
     @user = current_user
     unless @user.bank.present? && @user.branch.present? && @user.account_type.present? && @user.account_number.present? && @user.account_holder.present?
-      flash[:warning] = '口座情報を入力してください'
+      flash[:warning] = 'ポイント出金先口座を入力してください'
       redirect_to edit_user_url(current_user)
     end
   end
