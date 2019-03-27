61d816231c46e34a0420db5b2f83c5b476b4e595
okb <okb.for.git@gmail.com>
2019-03-27 16:32:33 +0000

    reset password migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190325095115) do
+ActiveRecord::Schema.define(version: 20190327163027) do
 
   create_table "available_instruments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "lesson_id"
@@ -124,11 +124,11 @@ ActiveRecord::Schema.define(version: 20190325095115) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "email",                       null: false
+    t.string   "email",                                           null: false
     t.string   "crypted_password"
     t.string   "salt"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
     t.string   "name"
     t.string   "bank"
     t.string   "branch"
@@ -138,8 +138,13 @@ ActiveRecord::Schema.define(version: 20190325095115) do
     t.string   "activation_state"
     t.string   "activation_token"
     t.datetime "activation_token_expires_at"
+    t.string   "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "reset_password_email_sent_at"
+    t.integer  "access_count_to_reset_password_page", default: 0
     t.index ["activation_token"], name: "index_users_on_activation_token", using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", using: :btree
   end
 
   create_table "users_languages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
