b50c4ac4d470e3c1fd4a9c3b9cb27b8e20a77832
okb <okb.for.git@gmail.com>
2019-03-27 18:09:49 +0000

    modify reset_password_email method in user_mailer.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -26,7 +26,7 @@ class UserMailer < ApplicationMailer
   
   def reset_password_email(user)
     @user = User.find user.id
-    @url = "https://desolate-taiga-73453.herokuapp.com" + edit_password_reset_path(@user.reset_password_token)
+    @url = "https://desolate-taiga-73453.herokuapp.com" + edit_password_reset_url(@user.reset_password_token)
     mail(:to => user.email,  :subject => "Your password has been reset")
   end
 end
