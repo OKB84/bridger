3d78975c82f007871b42f94e7eb2e5cab18e618c
okb <okb.for.git@gmail.com>
2019-04-03 17:14:35 +0000

    modify design of favorite button

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,14 @@
 class FavoritesController < ApplicationController
   def create
     instructor = User.find(params[:favorite_id])
-    current_user.favorite(instructor)
+    unless current_user.favorite?(instructor)
+      current_user.favorite(instructor)
+      respond_to do |format|
+        format.html { redirect_to request.referrer || root_url }
+        format.js
+      end
+      @lessons.reload
+    end
   end
 
   def destroy
