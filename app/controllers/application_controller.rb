6018484002f784df9ebad1bf2044947137bb9b57
okb <okb.for.git@gmail.com>
2019-04-03 14:54:49 +0000

    add counts method to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :require_login
   
+  def counts(user)
+    @count_favorited_users = user.favorited_users.count
+  end
+  
   private
   
   def not_authenticated
