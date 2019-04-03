4daa936c053978f832637d537ae37a74cfbbef79
okb <okb.for.git@gmail.com>
2019-04-03 14:50:42 +0000

    create and destroy method on favorites controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,11 @@
 class FavoritesController < ApplicationController
   def create
+    instructor = User.find(params[:favorite_id])
+    current_user.favorite(instructor)
   end
 
   def destroy
+    instructor = User.find(params[:favorite_id])
+    current_user.unfavorite(instructor)
   end
 end
