964d02d5cec1683d2079e308dc8a3f0163635beb
okb <okb.for.git@gmail.com>
2019-04-04 17:13:41 +0000

    Ajax

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,18 +1,15 @@
 class FavoritesController < ApplicationController
   def create
-    instructor = User.find(params[:favorite_id])
-    unless current_user.favorite?(instructor)
-      current_user.favorite(instructor)
-      respond_to do |format|
-        format.html { redirect_to request.referrer || root_url }
-        format.js
-      end
-      @lessons.reload
+    @instructor = User.find(params[:favorite_id])
+    unless current_user.favorite?(@instructor)
+      current_user.favorite(@instructor)
     end
+    @instructor.reload
   end
 
   def destroy
-    instructor = User.find(params[:favorite_id])
-    current_user.unfavorite(instructor)
+    @instructor = User.find(params[:favorite_id])
+    current_user.unfavorite(@instructor)
+    @instructor.reload
   end
 end
