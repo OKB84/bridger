98a661569a54d478393a71e1b9de4f7789bf35c0
okb <okb.for.git@gmail.com>
2019-04-07 12:33:03 +0000

    top instructors on toppage

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,8 +1,11 @@
 class ToppagesController < ApplicationController
   skip_before_action :require_login
+  include LessonsHelper
+  
   def index
     @latest_lesson = Lesson.last
     @second_latest_lesson = Lesson.last(2)[0]
     @third_latest_lesson = Lesson.last(3)[0]
+    @top_rated_lessons = Lesson.all.order(average_rate: "DESC").limit(6)
   end
 end
